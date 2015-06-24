71e6d5ba203b290487680c5c26dda6c660072f94
LyDong <dongquely@yahoo.com>
2015-06-24 16:11:50 +0200

    ...

--- MeinBudget/src/Registrierung.java
+++ MeinBudget/src/Registrierung.java
@@ -1,24 +1,36 @@
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Window;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 
 
 public class Registrierung extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
+	private JTextField txtBenutzername;
+	private JPasswordField PW1;
+	private JPasswordField PW2;
 
 	/**
 	 * Launch the application.
@@ -57,53 +69,70 @@ public class Registrierung extends JFrame {
 		lblBenutzername.setBounds(50, 60, 84, 14);
 		contentPane.add(lblBenutzername);
 		
-		JLabel lblPasswort = new JLabel("Passwort:");
-		lblPasswort.setBounds(50, 101, 84, 14);
-		contentPane.add(lblPasswort);
+		JLabel lblPW1 = new JLabel("Passwort:");
+		lblPW1.setBounds(50, 101, 84, 14);
+		contentPane.add(lblPW1);
 		
-		JLabel lblPasswortWiederholen = new JLabel("Passwort wiederholen:");
-		lblPasswortWiederholen.setBounds(50, 140, 124, 14);
-		contentPane.add(lblPasswortWiederholen);
+		JLabel lblPW2 = new JLabel("Passwort wiederholen:");
+		lblPW2.setBounds(50, 140, 124, 14);
+		contentPane.add(lblPW2);
 		
-		textField = new JTextField();
-		textField.setBounds(200, 57, 180, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtBenutzername = new JTextField();
+		txtBenutzername.setBounds(200, 57, 180, 20);
+		contentPane.add(txtBenutzername);
+		txtBenutzername.setColumns(10);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(200, 98, 180, 20);
-		contentPane.add(passwordField);
+		PW1 = new JPasswordField();
+		PW1.setBounds(200, 98, 180, 20);
+		contentPane.add(PW1);
 		
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(200, 137, 180, 20);
-		contentPane.add(passwordField_1);
+		PW2 = new JPasswordField();
+		PW2.setBounds(200, 137, 180, 20);
+		contentPane.add(PW2);
 		
-		//Registerobjekt deklarieren
+		
+		/* //Registerobjekt deklarieren
 		private Benutzername einBenutzername;
-						
+								
 		//Registerobjekt instanziieren
-		private einBenutzername = new Benutzername();
+		private einBenutzername = new Benutzername(); */
 		
+		
+		//Registrieren mit Mausklick oder Leertaste
 		JButton btnRegistrieren = new JButton("Registrieren");
-		btnRegistrieren.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				registrieren ();
+		btnRegistrieren.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				registrieren();
 			}
-		});
-		btnRegistrieren.setBounds(70, 196, 104, 23);
+
+			private void registrieren() {
+				String einBenutzername;
+				einBenutzername.setName(txtBenutzername.getText());
+			}
+			
+		/*//Benutzernamendaten zur Laufzeit speichern
+		//Eingabefehler prüfen
+			private void registrieren() {
+				einBenutzername.setName(txtBenutzer.getText());
+				
+				 if(txtBenutzer.getText().equals("")) {
+					fehlermeldung ("Text eingeben!");
+				}
+				else {
+					einBenutzername.setName(txtName.getTest());
+				} */
+			});
+		btnRegistrieren.setBounds(70, 200, 104, 23);
 		contentPane.add(btnRegistrieren);
 		
-		//Benutzernamendaten zur Laufzeit speichern
-		public void registrieren (){
-			einBenutzername.setName(txtBenutzer.getText());
-		}
-		
-		JButton Abbrechen = new JButton("Abbrechen");
-		Abbrechen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		//Abbrechen mit Mausklick
+		JButton btnAbbrechen = new JButton("Abbrechen");
+		btnAbbrechen.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
 			}
 		});
-		Abbrechen.setBounds(262, 196, 104, 23);
-		contentPane.add(Abbrechen);
+		btnAbbrechen.setBounds(260, 200, 104, 23);
+		contentPane.add(btnAbbrechen);
 	}
 }
